178cc32e688c8d70efbefd4a5fd3aece6bc248bd
Abdulqudoos-dot <quddoos18@gmail.com>
2024-02-19 20:20:20 +0500

    navbar

--- components/addForm/index.js
+++ components/addForm/index.js
@@ -40,7 +40,7 @@ const AddForm = () => {
         "https://fine-teal-zebra-kilt.cyclic.app/api/v1/bank/addBank",
         formData
       );
-      router.push("/", { scroll: false });
+      router.push("pages/home", { scroll: false });
     } catch (error) {
       console.error("Error submitting form:", error);
       alert("Error submitting form. Please try again later.");
