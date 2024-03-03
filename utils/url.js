d4a10d1c50dd376a53c1d1e89ebb5eeebff27a9c
Abdulqudoos-dot <quddoos18@gmail.com>
2024-03-03 22:40:22 +0500

    add new backend uri

--- utils/url.js
+++ utils/url.js
@@ -1,5 +1,5 @@
 //  depUrl = "https://banking-back-end-576fa7a10145.herokuapp.com";
 //  localUrl = "http://localhost:5000";
 
-const url = "https://banking-back-end-576fa7a10145.herokuapp.com";
+const url = "https://tallydash-tallybackend.0cpncy.easypanel.host";
 module.exports = url;
