700e0ad653686f0e83726896cabaa47bd362df2c
Abdulqudoos-dot <quddoos18@gmail.com>
2024-03-05 19:08:55 +0500

    add the all user bank transaction sho in admin

--- utils/url.js
+++ utils/url.js
@@ -1,5 +1,5 @@
 //  depUrl = "https://tallydash-tallybackend.0cpncy.easypanel.host";
 //  localUrl = "http://localhost:5000";
 
-const url = "http://localhost:5000";
+const url = "https://tallydash-tallybackend.0cpncy.easypanel.host";
 module.exports = url;
